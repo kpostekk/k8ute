1c775470416ab4a93db7186568e6e6434c693cee
Krystian Postek <s25290@pjwstk.edu.pl>
2025-01-07 01:03:35 +0100

    feat: implement dynamic flag service

--- src/challenges/secret-flags/secret-flags.module.ts
+++ src/challenges/secret-flags/secret-flags.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { SecretFlagsService } from './secret-flags.service';
-import { SecretFlagsResolver } from './secret-flags.resolver';
+import { Module } from "@nestjs/common"
+import { SecretFlagsService } from "./secret-flags.service"
+import { SecretFlagsResolver } from "./secret-flags.resolver"
+import { PrismaModule } from "src/prisma/prisma.module"
 
 @Module({
-  providers: [SecretFlagsService, SecretFlagsResolver]
+  imports: [PrismaModule],
+  providers: [SecretFlagsService, SecretFlagsResolver],
 })
 export class SecretFlagsModule {}
--- src/challenges/secret-flags/secret-flags.service.ts
+++ src/challenges/secret-flags/secret-flags.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common"
+import type { ChallengeDefinition } from "../collection/collection.service"
+import type { Challenger } from "@prisma/client"
+import { randomBytes } from "node:crypto"
+import { PrismaService } from "src/prisma/prisma.service"
 
 @Injectable()
-export class SecretFlagsService {}
+export class SecretFlagsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createFlag(
+    challenge: Pick<ChallengeDefinition, "id">,
+    challenger: Pick<Challenger, "id">,
+  ) {
+    const randomPayload = randomBytes(24).toString("base64url")
+    const flag = `k8ute_${randomPayload}`
+
+    const newFlag = await this.prisma.challengeDynamicFlag.upsert({
+      where: {
+        challengeId_challengerId: {
+          challengeId: challenge.id,
+          challengerId: challenger.id,
+        },
+      },
+      update: { flag },
+      create: {
+        challengeId: challenge.id,
+        challengerId: challenger.id,
+        flag,
+      },
+    })
+
+    return newFlag.flag
+  }
+
+  async validateChallengeFlag(
+    challenge: Pick<ChallengeDefinition, "id">,
+    challenger: Pick<Challenger, "id">,
+    flag: string,
+  ) {
+    const flagExists = await this.prisma.challengeDynamicFlag.findFirst({
+      where: {
+        challengeId: challenge.id,
+        challengerId: challenger.id,
+        flag,
+      },
+    })
+
+    return !!flagExists
+  }
+}
