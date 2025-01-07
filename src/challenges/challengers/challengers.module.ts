1b28d71847cafc9f4131e402d769d16708dd1455
Krystian Postek <s25290@pjwstk.edu.pl>
2025-01-07 01:08:36 +0100

    feat: update injected services into challengers module

--- src/challenges/challengers/challengers.module.ts
+++ src/challenges/challengers/challengers.module.ts
@@ -1,11 +1,30 @@
 import { Module } from "@nestjs/common"
 import { ChallengersService } from "./challengers.service"
 import { ChallengersGuard } from "./challengers.guard"
-import { RedisModule } from "src/redis/redis.module"
+import { PrismaModule } from "src/prisma/prisma.module"
+import { ChallengersResolver } from "./challengers.resolver"
+import { ChallengerAuthService } from "./challenger-auth/challenger-auth.service"
+import { JwtModule } from "@nestjs/jwt"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 
 @Module({
   // imports: [RedisModule],
-  providers: [ChallengersService, ChallengersGuard],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get("NEST_JWT_SECRET"),
+      }),
+    }),
+  ],
+  providers: [
+    ChallengersService,
+    ChallengersGuard,
+    ChallengersResolver,
+    ChallengerAuthService,
+  ],
   exports: [ChallengersService, ChallengersGuard],
 })
 export class ChallengersModule {}
